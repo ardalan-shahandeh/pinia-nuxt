c637848dd6eedd7bfc7e231bf150ca089a6e707a
Ardalan Shahandeh <43750987+ardalan-shahandeh@users.noreply.github.com>
2023-01-01 12:00:24 +0330

    Add files via upload
    npm install -g json-server
    
    json-server -w ./data/db.json

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,54 @@
   <main>
     <!-- heading -->
     <header>
-      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
-      <h1>{{ taskStore.name }}</h1>
+      <img src="./assets/monkey.webp" alt="pinia logo" width="75" height="75" />
+      <h1> Ardalan </h1>
     </header>
 
     <!-- new task form -->
 
-    <div>
+    <div class="addTask">
       <taskForm />
     </div>
 
     <!-- filter -->
 
     <nav>
-      <button @click="filter = 'all'">All tasks</button>
-      <button @click="filter = 'favs'">Favs tasks</button>
+      <button @click="filter = 'all'" class="button-62">All tasks</button>
+      <button @click="filter = 'favs'" class="button-62">Favs tasks</button>
     </nav>
+
+    <!-- loading -->
+
+    <div v-if="loading" class="loading">
+      Loading tasks ...
+    </div>
 
     <!-- task list -->
 
     <div class="task-list" v-if="filter === 'all'">
-      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>Your have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <taskDetails :task="task" />
       </div>
     </div>
-
-    <hr />
-    <hr />
 
     <div class="task-list" v-if="filter === 'favs'">
-      <p>Your have {{ taskStore.favCount }} favs left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>Your have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <taskDetails :task="task" />
       </div>
     </div>
+
+    <div class="reset-row">
+      <button @click="taskStore.$reset" class="button-62"> reset state </button>
+    </div>
+
   </main>
 </template>
 
 <script>
+import { storeToRefs } from 'pinia';
 import { ref } from "vue";
 import taskDetails from "@/components/TaskDetails.vue";
 import taskForm from "@/components/TaskForm.vue";
@@ -51,9 +60,13 @@ export default {
   setup() {
     const taskStore = useTaskStore();
 
+    const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore)
+    // fetch task
+    taskStore.getTasks()
+
     const filter = ref("all");
 
-    return { taskStore, filter };
+    return { taskStore, filter ,tasks, loading, favs, totalCount, favCount };
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -2,11 +2,8 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
-    tasks: [
-      {id:1, title:"buy some milk", isFav: false},
-      {id:2, title: "play Gloomhaven", isFav: true}
-    ],
-    name: 'Yoshi'
+    tasks: [],
+    isLoading: false
   }),
   getters: {
     favs() {
@@ -22,18 +19,52 @@ export const useTaskStore = defineStore('taskStore', {
     }
   },
   actions: {
-    addTask(task) {
+    async getTasks() {
+      this.loading = true
+      const res = await fetch('http://localhost:3000/tasks')
+      const data  = await res.json()
+      
+      this.tasks = data
+      this.loading = false
+    },
+    async addTask(task) {
       this.tasks.push(task)
+
+      const res = await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {'content-type': 'application/json'}
+      })
+
+      if(res.error) {
+        console.log(res.error);
+      }
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       this.tasks = this.tasks.filter(t => {
-        console.log(id);
         return t.id !== id
-      }) 
+      })
+      const res = await fetch('http://localhost:3000/tasks/' + id, {
+        method: 'DELETE'
+      })
+
+      if(res.error) {
+        console.log(res.error);
+      }
     },
-    toggleFave(id) {
+    async toggleFave(id) {
       const task = this.tasks.find(t => t.id === id)      
       task.isFav = !task.isFav
+
+      const res = await fetch('http://localhost:3000/tasks' + id, {
+        method: 'PATCH',
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: {'content-type': 'application/json'}
+      })
+
+      if(res.error) {
+        console.log(res.error);
+      }
     }
   }
 })
